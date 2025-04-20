ae74d46209d98291ce6b6153b565bf5b3c890e1b
Nils Linnhöfer <consulting@linnhoefer.de>
2025-04-20 16:10:43 +0200

    feat: accept query and sort update fn
    Closes #33

--- projects/ngx-pagination-data-source/src/lib/pagination-data-source.ts
+++ projects/ngx-pagination-data-source/src/lib/pagination-data-source.ts
@@ -1,67 +1,78 @@
-import {SimpleDataSource} from './simple-data-source'
-import {Page, PaginationEndpoint, Sort} from './page'
-import {indicate} from './indicate'
-import {BehaviorSubject, combineLatest, Observable, Subject} from 'rxjs';
-import {map, shareReplay, startWith, switchMap, tap} from 'rxjs/operators';
+import { SimpleDataSource } from './simple-data-source';
+import { Page, PaginationEndpoint, Sort } from './page';
+import { indicate } from './indicate';
+import { BehaviorSubject, combineLatest, Observable, Subject } from 'rxjs';
+import { map, shareReplay, startWith, switchMap, tap } from 'rxjs/operators';
 
+export type UpdateFn<T> = (last: T) => T;
 
-export class PaginationDataSource<T, Q = Partial<T>> implements SimpleDataSource<T> {
-  public loading$: Observable<boolean>
-  public page$: Observable<Page<T>>
+export class PaginationDataSource<T, Q = Partial<T>>
+  implements SimpleDataSource<T>
+{
+  public loading$: Observable<boolean>;
+  public page$: Observable<Page<T>>;
 
-  private readonly pageNumber = new Subject<number>()
-  private readonly sort: BehaviorSubject<Sort<T>>
-  private readonly query: BehaviorSubject<Q>
-  private readonly loading = new Subject<boolean>()
+  private readonly pageNumber = new Subject<number>();
+  private readonly sort: BehaviorSubject<Sort<T>>;
+  private readonly query: BehaviorSubject<Q>;
+  private readonly loading = new Subject<boolean>();
 
   constructor(
     private endpoint: PaginationEndpoint<T, Q>,
     initialSort: Sort<T>,
     initialQuery: Q,
     public pageSize = 20,
-    public initialPage = 0,
-    ) {
+    public initialPage = 0
+  ) {
     let firstCall = true;
-    this.query = new BehaviorSubject<Q>(initialQuery)
-    this.sort = new BehaviorSubject<Sort<T>>(initialSort)
-    const param$ = combineLatest([this.query, this.sort])
-    this.loading$ = this.loading.asObservable()
+    this.query = new BehaviorSubject<Q>(initialQuery);
+    this.sort = new BehaviorSubject<Sort<T>>(initialSort);
+    const param$ = combineLatest([this.query, this.sort]);
+    this.loading$ = this.loading.asObservable();
     this.page$ = param$.pipe(
-      switchMap(([query, sort]) => this.pageNumber.pipe(
-        startWith(initialPage && firstCall ? initialPage : 0),
-        tap(() => firstCall = false),
-        switchMap(page => this.endpoint({page, sort, size: this.pageSize}, query)
-          .pipe(indicate(this.loading))
+      switchMap(([query, sort]) =>
+        this.pageNumber.pipe(
+          startWith(initialPage && firstCall ? initialPage : 0),
+          tap(() => (firstCall = false)),
+          switchMap((page) =>
+            this.endpoint({ page, sort, size: this.pageSize }, query).pipe(
+              indicate(this.loading)
+            )
+          )
         )
-      )),
+      ),
       shareReplay(1)
-    )
+    );
   }
 
-  sortBy(sort: Partial<Sort<T>>): void {
-    const lastSort = this.sort.getValue()
-    const nextSort = {...lastSort, ...sort}
-    this.sort.next(nextSort)
+  sortBy(sortUpdate: Partial<Sort<T>> | UpdateFn<Sort<T>>): void {
+    const lastSort = this.sort.getValue();
+    const nextSort =
+      typeof sortUpdate === 'function'
+        ? sortUpdate(lastSort)
+        : { ...lastSort, ...sortUpdate };
+    this.sort.next(nextSort);
   }
 
-  queryBy(query: Partial<Q>): void {
-    const lastQuery = this.query.getValue()
-    const nextQuery = {...lastQuery, ...query}
-    this.query.next(nextQuery)
+  queryBy(queryUpdate: Partial<Q> | UpdateFn<Q>): void {
+    const lastQuery = this.query.getValue();
+    const nextQuery =
+      typeof queryUpdate === 'function'
+        ? queryUpdate(lastQuery)
+        : { ...lastQuery, ...queryUpdate };
+    this.query.next(nextQuery);
   }
 
   fetch(page: number, pageSize?: number): void {
     if (pageSize) {
-      this.pageSize = pageSize
+      this.pageSize = pageSize;
     }
-    this.pageNumber.next(page)
+    this.pageNumber.next(page);
   }
 
   connect(): Observable<T[]> {
-    return this.page$.pipe(map(page => page.content))
+    return this.page$.pipe(map((page) => page.content));
   }
 
-  disconnect(): void {
-  }
-
+  disconnect(): void {}
 }
